e6a6361d4473e93d62b4b99966a591425b2c68d1
Rodrigo Legarreta <15068@ulsachihuahua.edu.mx>
2025-11-27 09:37:37 -0600

    se corrige el index y vercel

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-//ruta 1
-app.get('/mensaje1', (req, res) => {
-  res.send('bienvenidos!');
+// Ruta raíz: muestra nombres del equipo
+app.get('/', (req, res) => {
+  res.send('Integrantes del equipo: Rodrigo Legarreta, [agrega los demás]');
 });
 
-//ruta 2  
+// /ulsa: misión de la ULSA
 app.get('/ulsa', (req, res) => {
-  res.send('forar alumnos con valor');
+  res.send('Misión de la ULSA: Formar alumnos con valor.');
 });
 
-//iniciar servidor 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// /saludar/{nombre}: saludar dinámicamente
+app.get('/saludar/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send(`Hola ${nombre}!`);
 });
+
+// Exportar para que Vercel lo use como servidorless function
+module.exports = app;
